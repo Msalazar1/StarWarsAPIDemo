d2fb802a96ff11123f87e0d1545f91f170b443db
Manny <mannysalazar@quickenloans.com>
2020-10-13 14:25:44 -0400

    added API calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PeopleViewComponent } from './people-view/people-view.component';
+import { PersonViewComponent } from './person-view/person-view.component';
+import { PlanetViewComponent } from './planet-view/planet-view.component';
+import { FilmViewComponent } from './film-view/film-view.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleViewComponent,
+    PersonViewComponent,
+    PlanetViewComponent,
+    FilmViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
